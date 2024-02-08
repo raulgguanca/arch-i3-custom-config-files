60e180744c1a66ab1efd4da737aecbc0ad991e9d
raulgguanca <raulgguanca@gmail.com>
2024-02-08 20:08:55 -0300

    neovim pre customization script

---
+++ config-nvim.sh
@@ -0,0 +1,19 @@
+#! bin/bash
+
+mkdir -p ~/.vim/plugged
+
+sudo mkdir -p /root/.config/nvim
+
+sudo mkdir -p /root/.vim/plugged
+
+sudo cp ~/.config/nvim/init.vim /root/.config
+
+sudo cp ~/.config/nvim/coc-settings.json /root/.config
+
+#https://github.com/junegunn/vim-plug
+
+sudo sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+exit 0
