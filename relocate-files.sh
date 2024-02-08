8f206931c91e9870a24097833a41d8fce79cc138
raulgguanca <raulgguanca@gmail.com>
2024-02-08 19:04:58 -0300

    script to relocate files

---
+++ relocate-files.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+mv .alacritty.toml .bash_aliases .bashrc .general-settings.txt .screenlayout .vim .xmodmap start.sh themes.sh ~/tempx
+
+sudo mv i3files/config ~/.config/i3/config
+
+sudo mv i3files/i3status.conf /etc/i3status.conf
+
+mv nvim ~/.config
+
+sudo mv touchpad-tap.conf /etc/X11/xorg.conf.d/touchpad-tap.conf
+
+cd .. && rm -rf arch-i3-custom-config-files
+
+exit 0
