d11bda474c1fe6a1167356a599738ca3e2e8ff21
raulgguanca <raulgguanca@gmail.com>
2024-03-18 15:40:05 -0300

    config to streamlink

--- basic-programs.sh
+++ basic-programs.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
-pacman -S networkmanager git xorg xorg-apps xorg-xkill xorg-xclock xorg-drivers firefox discord code thunderbird wpa_supplicant wireless_tools netctl dialog neofetch htop glances zip unzip numlockx base-devel iwd ntfs-3g dhcpcd xorg-xinit nitrogen picom alacritty  pcmanfm leafpad krita lxappearance gparted gnome-screenshot gnome-disk-utility arandr cheese rofi alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-jack i3 feh nodejs npm tmux  xclip unrar connman ranger w3m locate p7zipsd amd/intel-ucode meld tree rkhunter vlc mpv yt-dlp
+pacman -S networkmanager git xorg xorg-apps xorg-xkill xorg-xclock xorg-drivers firefox discord code thunderbird wpa_supplicant wireless_tools netctl dialog neofetch htop glances zip unzip numlockx base-devel iwd ntfs-3g dhcpcd xorg-xinit nitrogen picom alacritty  pcmanfm leafpad krita lxappearance gparted gnome-screenshot gnome-disk-utility arandr cheese rofi alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-jack i3 feh nodejs npm tmux  xclip unrar connman ranger w3m locate p7zipsd amd/intel-ucode meld tree rkhunter vlc mpv yt-dlp streamlink
 
 systemctl enable dhcpcd
 
