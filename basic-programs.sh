4322be889fa8a6683ddd46763b29c9a7438d8ea6
raulgguanca <raulgguanca@gmail.com>
2024-07-30 13:29:31 -0300

    added monitor settings

--- basic-programs.sh
+++ basic-programs.sh
@@ -1,6 +1,6 @@
 #! bin/bash
 
-pacman -S amd/intel-ucode networkmanager xorg xorg-apps xorg-xkill xorg-xclock xorg-drivers firefox discord code thunderbird wpa_supplicant wireless_tools netctl dialog neofetch htop glances zip unzip numlockx base-devel iwd ntfs-3g dhcpcd xorg-xinit nitrogen picom alacritty pcmanfm leafpad krita lxappearance gparted gnome-screenshot gnome-disk-utility arandr cheese rofi alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-jack i3 feh nodejs npm tmux xclip unrar connman ranger w3m locate p7zip meld tree rkhunter vlc mpv yt-dlp streamlink mariadb openssh
+pacman -S amd/intel-ucode networkmanager xorg xorg-apps xorg-xkill xorg-xclock xorg-drivers firefox discord code thunderbird wpa_supplicant wireless_tools netctl dialog neofetch htop glances zip unzip numlockx base-devel iwd ntfs-3g dhcpcd xorg-xinit nitrogen picom alacritty pcmanfm leafpad krita lxappearance gparted gnome-screenshot gnome-disk-utility arandr cheese rofi alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-jack i3 feh nodejs npm tmux xclip unrar connman ranger w3m locate p7zip meld tree rkhunter vlc mpv yt-dlp streamlink mariadb openssh libreoffice
 
 systemctl enable dhcpcd
 
