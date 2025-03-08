d1bf7c0228e7f91aace9620ac5bb453226f5a836
raulgguanca <raulgguanca@gmail.com>
2025-03-08 20:17:55 -0300

    optimized files

--- 2-post-login-relocate-files.sh
+++ 2-post-login-relocate-files.sh
@@ -1,20 +1,12 @@
 #! /bin/bash
 
-cp /etc/X11/xinit/xinitrc /home/raul/.xinitrc
-
-echo 'picom &' >> /home/raul/.xinitrc
-
-echo 'numlockx &' >> /home/raul/.xinitrc
-
-echo 'exec i3' >> /home/raul/.xinitrc
-
 mv .alacritty.toml .bash_aliases .bashrc .general-settings.txt .screenlayout .xmodmap start.sh themes.sh config-nvim.sh ~/
 
 sudo mv i3files/config ~/.config/i3/config
 
 sudo mv i3files/i3status.conf /etc/i3status.conf
 
-mv nvim ~/.config
+mv nvim ~/.config/nvim
 
 sudo mv touchpad-tap.conf /etc/X11/xorg.conf.d/touchpad-tap.conf
 
