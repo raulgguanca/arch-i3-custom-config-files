253cb25bd6bd4e0e273c85a260fd433715d86289
raulgguanca <raulgguanca@gmail.com>
2025-08-05 09:26:12 -0300

    changed caps lock and escape key settings to be persistent on x11

--- start.sh
+++ start.sh
@@ -7,8 +7,6 @@
 xrandr --output eDP1 --brightness 0.7 --gamma 1:0.88:0.76 #&& xrandr --output VGA1 --brightness 0.7 --gamma 1:0.88:0.76 && xrandr --output HDMI1 --brightness 0.7 --gamma 1:0.88:0.76
 #nmcli dev down wlan0
 
-xmodmap .xmodmap
-
 xset s off && xset -dpms
 
 exit 0
