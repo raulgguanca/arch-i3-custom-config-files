f5b8093cdd236fe6e0e1448298e6421d47cbc754
raulgguanca <raulgguanca@gmail.com>
2025-08-08 20:05:06 -0300

    new improved logic

--- 1-basic-programs.sh
+++ 1-basic-programs.sh
@@ -1,4 +1,5 @@
 #! bin/bash
+##provide the user name before running
 
 pacman -S intel-ucode networkmanager xorg xorg-apps xorg-xkill xorg-xclock xorg-drivers firefox discord code thunderbird wpa_supplicant wireless_tools netctl dialog fastfetch htop glances zip unzip numlockx base-devel iwd ntfs-3g dhcpcd xorg-xinit nitrogen picom alacritty pcmanfm leafpad krita lxappearance gparted gnome-screenshot gnome-disk-utility arandr cheese rofi alsa-utils alsa-plugins pulseaudio pulseaudio-alsa pulseaudio-jack i3 feh nodejs npm tmux xclip unrar connman ranger w3m locate p7zip meld tree rkhunter vlc mpv yt-dlp streamlink mariadb openssh libreoffice nftables tlp flameshot qt5-imageformats noto-fonts noto-fonts-cjk noto-fonts-emoji vlc-plugin-ffmpeg #vlc-plugins-all ttf-jigmo
 
@@ -10,13 +11,13 @@ systemctl enable nftables
 
 systemctl enable tlp.service
 
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
+cp /etc/X11/xinit/xinitrc /home/$1/.xinitrc
 
-echo 'picom &' >> ~/.xinitrc
+echo 'picom &' >> /home/$1/.xinitrc
 
-echo 'numlockx &' >> ~/.xinitrc
+echo 'numlockx &' >> /home/$1/.xinitrc
 
-echo 'exec i3' >> ~/.xinitrc
+echo 'exec i3' >> /home/$1/.xinitrc
 
 ##Ranger
 ranger --copy-config=all
